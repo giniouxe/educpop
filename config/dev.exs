8c55ddaacc288d2f6b79e99818961412624db517
Giniouxe <aurelie.ginioux@gmail.com>
2018-07-22 22:27:28 +0200

    Update dev database config

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :educpop, Educpop.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "educ_pop",
+  password: "SomeIncrediblySecurePassword",
   database: "educpop_dev",
   hostname: "localhost",
   pool_size: 10
